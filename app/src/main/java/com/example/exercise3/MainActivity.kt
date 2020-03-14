eda7ac7744715b95962117e22d801f2a108cec64
Hyperhawk69 <rlcabuslay1@up.edu.ph>
2020-03-14 18:50:23 +0800

    feat: cut up button and options menu

--- app/src/main/java/com/example/exercise3/MainActivity.kt
+++ app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -28,21 +28,7 @@ class MainActivity : AppCompatActivity() {
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        drawerLayout = binding.drawerLayout
-
-        val navController = this.findNavController(R.id.myNavHostFragment)
-
-        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
-
-        NavigationUI.setupWithNavController(binding.navView, navController)
-
     }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
-    }
-
 
 
 }
