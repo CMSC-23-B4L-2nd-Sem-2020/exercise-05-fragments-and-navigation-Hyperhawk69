295930915e8788036c511152195f8c9bfbcc36d8
Hyperhawk69 <rlcabuslay1@up.edu.ph>
2020-03-15 13:49:49 +0800

    feat: reset finalCount to 0 on back press

--- app/src/main/java/com/example/exercise3/MainActivity.kt
+++ app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -1,33 +1,26 @@
 //Cabuslay, Ryan Vincent L.
 //2018-12076
-//02-26-2020
+//03-19-2020
 
 package com.example.exercise3
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
-import 	androidx.fragment.app.FragmentActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import com.example.exercise3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        GameFragment.finalCount = 0
     }
 
 }
